7a918074b0dbda32f66b9fc34a4a68fbaf5acb7b
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-06 18:29:03 +0200

    fix: change namming

--- src/app/(consultant)/profile/page.tsx
+++ src/app/(consultant)/profile/page.tsx
@@ -3,7 +3,7 @@ type DashboardProps = {
 };
 
 const Dashboard = ({ children }: DashboardProps) => {
-  return <h1>dashboard{children}</h1>;
+  return <h1>Page profile{children}</h1>;
 };
 
 export default Dashboard;
