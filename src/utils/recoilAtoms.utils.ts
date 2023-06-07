3ceb6efd6b51d16cc80e763f51ac95efd2720740
Milan <pro@turtletv.fr>
2023-06-07 10:59:45 +0200

    ✨ Improve token atom

--- src/utils/recoilAtoms.utils.ts
+++ src/utils/recoilAtoms.utils.ts
@@ -1,6 +1,21 @@
 import { atom } from "recoil";
 
-export const tokenState = atom({
-  key: "tokenState",
-  default: "",
+const isBrowser = typeof window !== "undefined";
+
+export const tokenAtom = atom({
+  key: "tokenAtom",
+  default: isBrowser ? localStorage.getItem("token") || null : null,
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet((newValue: string | null) => {
+        if (isBrowser) {
+          if (newValue) {
+            localStorage.setItem("token", newValue);
+          } else {
+            localStorage.removeItem("token");
+          }
+        }
+      });
+    },
+  ],
 });
