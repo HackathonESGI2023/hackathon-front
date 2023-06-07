857edffebc5f3767720d5e205ab4c52077879d7e
Milan <pro@turtletv.fr>
2023-06-07 11:44:45 +0200

    👽️ Add get user informations

--- src/app/api/Users/getUsers.ts
+++ src/app/api/Users/getUsers.ts
@@ -1,6 +1,42 @@
+import {
+  Contract,
+  Role,
+  Event,
+  Mission,
+  SkillLevel,
+  SkillType,
+  Workshop,
+} from "@prisma/client";
 import { GET } from "../route";
 
 export async function getUsers() {
-  const res = await GET(`${process.env.NEXT_PUBLIC_API_URL}/user/`);
+  const res = await GET(`http://localhost:3000/users`);
   return res.json();
+}
+
+export interface UserResponse {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  profile_picture: string;
+  address: string;
+  slackId: string;
+  roles: Role[];
+  Contract: Contract[];
+  Events: Event[];
+  EventsOwner: Event[];
+  Missions: Mission[];
+  UserSkill: {
+    id: number;
+    isStarred: boolean;
+    level: SkillLevel;
+    skill: {
+      color: string;
+      name: string;
+      type: SkillType;
+    };
+  }[];
+  WorkshopOwner: Workshop[];
+  Workshops: Workshop[];
 }
