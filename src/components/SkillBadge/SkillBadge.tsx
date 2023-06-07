a4aecc7165e0ab6ffd39d360ce50d7eff741d568
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-07 11:18:38 +0200

    feat: Add drag and drop source

--- src/components/SkillBadge/SkillBadge.tsx
+++ src/components/SkillBadge/SkillBadge.tsx
@@ -1,16 +1,33 @@
 'use client';
 
 import { motion } from 'framer-motion';
+import { useDrag } from 'react-dnd';
 import styles from './SkillBadge.module.scss';
 
 type SkillBadgeProps = {
   name: string;
   color: string;
+  id: any;
 };
 
-const SkillBadge = ({ name, color }: SkillBadgeProps) => {
-  return (
+const SkillBadge = ({ name, color, id }: SkillBadgeProps) => {
+  const [collected, drag, dragPreview] = useDrag(() => ({
+    type: 'skillBadge',
+    item: { id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  if (collected.isDragging) {
+    console.log('dragging 🚗');
+  }
+
+  return collected.isDragging ? (
+    <div ref={dragPreview}>Pouet preview</div>
+  ) : (
     <motion.div
+      ref={drag}
       className={styles.skillBadge}
       style={{ backgroundColor: color }}
       whileHover={{ scale: 1.1 }}
