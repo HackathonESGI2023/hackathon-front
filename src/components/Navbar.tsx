23296bd271493af69e3b8a67f6bad1f3efb4da87
SylvainBoudacher <sylvain.boudacher@gmail.com>
2023-06-07 11:26:07 +0200

    fix(navbar-&-home): useRouter of Next.js to change pages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
-'use client';
-import { Button, Link, Navbar, Switch, useTheme } from '@nextui-org/react';
-import { useTheme as useNextTheme } from 'next-themes';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+"use client";
+import { Button, Link, Navbar, Switch, useTheme } from "@nextui-org/react";
+import { useTheme as useNextTheme } from "next-themes";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 type NavbarTestProps = {
   className?: string;
 };
 
 const NavbarTest = ({ className }: NavbarTestProps) => {
-  const [variant, setVariant] = useState('default');
-  const [activeColor, setActiveColor] = useState('primary');
+  const [variant, setVariant] = useState("default");
+  const [activeColor, setActiveColor] = useState("primary");
 
   const { isDark } = useTheme();
   const router = useRouter();
@@ -47,23 +47,21 @@ const NavbarTest = ({ className }: NavbarTestProps) => {
       <Navbar.Content>
         <Switch
           checked={isDark}
-          onChange={(e) => setTheme(e.target.checked ? 'dark' : 'light')}
+          onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
         />
-        <Navbar.Link color="inherit" onClick={() => router.push('/admin')}>
+        <Navbar.Link color="inherit" onClick={() => router.push("/admin")}>
           ADMIN
         </Navbar.Link>
-        <Navbar.Link color="inherit" onClick={() => router.push('/login')}>
-          Login
-        </Navbar.Link>
+
         <Navbar.Item>
           <Button
             auto
             flat
             as={Link}
             color="primary"
-            onClick={() => router.push('/register')}
+            onClick={() => router.push("/login")}
           >
-            Sign Up
+            Login
           </Button>
         </Navbar.Item>
       </Navbar.Content>
