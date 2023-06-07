fe5668b52e04928d734a2a0d177d8666bcb99a00
SylvainBoudacher <sylvain.boudacher@gmail.com>
2023-06-08 00:06:21 +0200

    fix(profileCards): add props

--- src/app/(consultant)/components/Cards/ProfileCards/ProfileCards.tsx
+++ src/app/(consultant)/components/Cards/ProfileCards/ProfileCards.tsx
@@ -14,76 +14,101 @@ import {
   Tooltip,
 } from "@nextui-org/react";
 import { CaretRight, Confetti, Trophy } from "@phosphor-icons/react";
+import { ContractTypeEnum } from "@schemas/contract.schema";
+import { SkillsDto } from "@schemas/skills.schema";
 
-const ProfileCards = () => {
+interface TemplateProps {
+  user: any;
+  isInMision: boolean;
+  userContractType?: ContractTypeEnum;
+  skills: SkillsDto[];
+  seniorityTimeInYears: number;
+}
+
+const ProfileCards: React.FunctionComponent<TemplateProps> = ({
+  user,
+  isInMision,
+  userContractType,
+  skills,
+  seniorityTimeInYears,
+}) => {
   return (
-    <Card isPressable isHoverable>
-      <Card.Body>
-        <Row justify="flex-start" css={{ pt: "5px" }}>
-          <Grid md={7}>
-            <Avatar
-              size={"xl"}
-              squared
-              src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
-            />
-          </Grid>
-          <Grid md={15}>
-            <Col>
-              <Row justify="flex-start">
-                <Text>Charles Lambert</Text>
-              </Row>
-              <Row justify="flex-start">
-                <BadgeMission inMission={false} />
-                <BadgeContract contractType="FREELANCE" />
-              </Row>
-            </Col>
-          </Grid>
-          <Grid md={2}>
-            <CaretRight size={25} />
-          </Grid>
-        </Row>
-
-        <Col css={{ pt: "10px", mx: 13 }}>
-          <Row justify="flex-start">
-            <Grid.Container gap={1}>
-              <Grid>
-                <BadgeSkill color="#190633">React</BadgeSkill>
-              </Grid>
-              <Grid>
-                <BadgeSkill color="#d5e0a8">Next.js</BadgeSkill>
-              </Grid>
-              <Grid>
-                <Badge disableOutline color="secondary">
-                  Docker
-                </Badge>
-              </Grid>
-            </Grid.Container>
+    <>
+      <Card isPressable isHoverable>
+        <Card.Body>
+          <Row justify="flex-start" css={{ pt: "5px" }}>
+            <Grid md={7}>
+              <Avatar
+                size={"xl"}
+                squared
+                src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+              />
+            </Grid>
+            <Grid md={15}>
+              <Col>
+                <Row justify="flex-start">
+                  <Text>
+                    {user.firstname} {user.lastname}
+                  </Text>
+                </Row>
+                <Row justify="flex-start">
+                  <BadgeMission inMission={isInMision} />
+                  {userContractType && (
+                    <BadgeContract contractType={userContractType} />
+                  )}
+                </Row>
+              </Col>
+            </Grid>
+            <Grid md={2}>
+              <CaretRight size={25} />
+            </Grid>
           </Row>
-          <Row justify="flex-start">
-            <Grid.Container gap={1}>
-              <Grid>
-                <BadgeSeniority seniorityTime={3} />
-              </Grid>
-              <Grid>
-                <Tooltip content={"Meilleur employé"} rounded color="primary">
-                  <Badge color="error" content="2" shape="circle">
-                    <Trophy size={32} color="#8ca413" weight="fill" />
-                  </Badge>
-                </Tooltip>
-              </Grid>
 
-              <Grid>
-                <Tooltip content={"Meilleur employé"} rounded color="primary">
-                  <Badge color="error" content="2" shape="circle">
-                    <Confetti size={32} color="#d71414" weight="fill" />
-                  </Badge>
-                </Tooltip>
-              </Grid>
-            </Grid.Container>
-          </Row>
-        </Col>
-      </Card.Body>
-    </Card>
+          <Col css={{ pt: "10px", mx: 13 }}>
+            <Row justify="flex-start">
+              <Grid.Container gap={1}>
+                {/* <Grid>
+                  <BadgeSkill color="#190633">React</BadgeSkill>
+                </Grid>
+                <Grid>
+                  <BadgeSkill color="#d5e0a8">Next.js</BadgeSkill>
+                </Grid>
+                <Grid>
+                  <BadgeSkill color="#d5e0a8">Next.js</BadgeSkill>
+                </Grid> */}
+                {skills.map((skill) => (
+                  <Grid key={skill.id}>
+                    <BadgeSkill color="#190633">{skill.name}</BadgeSkill>
+                  </Grid>
+                ))}
+              </Grid.Container>
+            </Row>
+            <Row justify="flex-start">
+              <Grid.Container gap={1}>
+                <Grid>
+                  <BadgeSeniority seniorityTime={seniorityTimeInYears} />
+                </Grid>
+                <Grid>
+                  <Tooltip content={"Meilleur employé"} rounded color="primary">
+                    <Badge color="error" content="2" shape="circle">
+                      <Trophy size={32} color="#8ca413" weight="fill" />
+                    </Badge>
+                  </Tooltip>
+                </Grid>
+
+                <Grid>
+                  <Tooltip content={"Meilleur employé"} rounded color="primary">
+                    <Badge color="error" content="2" shape="circle">
+                      <Confetti size={32} color="#d71414" weight="fill" />
+                    </Badge>
+                  </Tooltip>
+                </Grid>
+              </Grid.Container>
+            </Row>
+          </Col>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
--- src/app/(consultant)/profile/page.tsx
+++ src/app/(consultant)/profile/page.tsx
@@ -2,6 +2,13 @@
 import { Grid } from "@nextui-org/react";
 import ProfileCards from "../components/Cards/ProfileCards/ProfileCards";
 
+//fake user
+const userTest = {
+  firstname: "John",
+  lastname: "Doe",
+  email: "test@test.com",
+};
+
 type DashboardProps = {
   children: React.ReactNode;
 };
@@ -12,16 +19,28 @@ const Dashboard = ({ children }: DashboardProps) => {
       <h1>Page profile{children}</h1>
       <Grid.Container gap={3} justify="center">
         <Grid xs={6} md={3}>
-          <ProfileCards />
-        </Grid>
-        <Grid xs={6} md={3}>
-          <ProfileCards />
-        </Grid>
-        <Grid xs={6} md={3}>
-          <ProfileCards />
-        </Grid>
-        <Grid xs={6} md={3}>
-          <ProfileCards />
+          <ProfileCards
+            isInMision={true}
+            seniorityTimeInYears={2}
+            user={userTest}
+            skills={[
+              {
+                _id: "1",
+                name: "React",
+                color: "#61DBFB",
+              },
+              {
+                _id: "2",
+                name: "Node",
+                color: "#026E00",
+              },
+              {
+                _id: "3",
+                name: "Angular",
+                color: "#DD0031",
+              },
+            ]}
+          />
         </Grid>
       </Grid.Container>
     </>
