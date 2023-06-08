20744803cdbbaa59a1ab38ffe79bfd5316190a69
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-08 17:06:15 +0200

    feat: add SkillCard component

--- src/app/(user)/dashboard/romain/page.tsx
+++ src/app/(user)/dashboard/romain/page.tsx
@@ -1,34 +1,17 @@
 "use client";
 
-import { Row, Spacer } from "@nextui-org/react";
-import { AirplaneTilt } from "@phosphor-icons/react";
-import colors from "@styles/_colors.module.scss";
-import KpiCard from "../components/KpiCard/KpiCard";
+import { Row } from "@nextui-org/react";
+import SkillsCard from "../components/SkillsCard/SkillsCard";
 
 type DashboardProps = {};
 
 const Dashboard = ({}: DashboardProps) => {
   return (
-    <Row>
-      <KpiCard
-        color={colors.terciaryT500}
-        contratsColor={colors.terciaryS500}
-        label="Entreprise"
-        unityLabel="Entreprises renseignées"
-        amount={586}
-      />
-      <Spacer x={1} />
-      <KpiCard
-        color={colors.secondaryT500}
-        contratsColor={colors.secondaryS500}
-        label="C.A"
-        unityLabel="Euros"
-        amount={586}
-        icon={
-          <AirplaneTilt size={65} weight="fill" color={colors.secondaryS500} />
-        }
-      />
-    </Row>
+    <>
+      <Row>
+        <SkillsCard></SkillsCard>
+      </Row>
+    </>
   );
 };
 
