9daba642e1313dc7d998f9201c585a2f0ac8a558
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-08 16:14:26 +0200

    feat: add KpiCard Component

---
+++ src/app/(user)/dashboard/components/KpiCard/KpiCard.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Card, Row, Spacer, Text } from "@nextui-org/react";
+import { Buildings } from "@phosphor-icons/react";
+import colors from "@styles/_colors.module.scss";
+import { motion } from "framer-motion";
+
+type KpiCardProps = {
+  color: string;
+  icon?: React.ReactNode;
+  label: string;
+  contratsColor?: string;
+  amount: number;
+  unityLabel: string;
+};
+
+const KpiCard = ({
+  color = colors.white,
+  icon,
+  label,
+  contratsColor = colors.primary,
+  amount,
+  unityLabel,
+}: KpiCardProps) => {
+  return (
+    <motion.div>
+      <Card
+        style={{
+          height: "100%",
+          width: "15rem",
+          background: color,
+          padding: "1.5rem",
+        }}
+        variant="flat"
+      >
+        <Row justify="space-between" align="center">
+          {icon ? (
+            icon
+          ) : (
+            <Buildings size={65} weight="fill" color={contratsColor} />
+          )}
+        </Row>
+        <Spacer y={0.75} />
+        <Row justify="space-between" align="center">
+          <Text h5 weight={"bold"} size={"$3xl"} color={colors.primary}>
+            {label}
+          </Text>
+        </Row>
+        <Spacer y={0.5} />
+        <Row align="center">
+          <Text h5 size={"$5xl"} color={contratsColor}>
+            {amount}
+          </Text>
+          <Spacer x={0.5} />
+          <Text h5 color={colors.primary}>
+            {unityLabel}
+          </Text>
+        </Row>
+      </Card>
+    </motion.div>
+  );
+};
+
+export default KpiCard;
