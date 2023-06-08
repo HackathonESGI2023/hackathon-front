97a5fba5d56562a8441e0fd7d4c9e752a574ecf8
Milan <pro@turtletv.fr>
2023-06-08 10:21:20 +0200

    🏷️ Type the API

--- src/app/api/Applications/createApplication.ts
+++ src/app/api/Applications/createApplication.ts
@@ -3,14 +3,16 @@ import { POST } from "../route";
 export type CreateApplicationRequest = Pick<
   Application,
   "name" | "text" | "email" | "cv" | "coverLetter"
-> & { sponsor: string };
+> & { sponsor?: string };
 
 /**
  * POST /applications
  * @body CreateApplicationRequest
  * @returns Application
  */
-export const createApplication = async (request: CreateApplicationRequest) => {
+export const createApplication = async (
+  request: CreateApplicationRequest
+): Promise<Application> => {
   const response = await POST("applications", request);
   return response.json();
 };
