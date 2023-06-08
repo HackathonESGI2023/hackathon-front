bbd61470981a3202e11669e5e23757bdc5a2a4ab
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-08 11:05:56 +0200

    fix: fix export

--- src/app/(consultant)/profile/page.tsx
+++ src/app/(consultant)/profile/page.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import SelectPinnedSkills from "./components/SelectPinnedSkills/SelectPinnedSkills";
-
 type ProfileProps = {};
 
 const Profile = ({}: ProfileProps) => {
   return (
     <>
       <h1>Profile page</h1>
-      <SelectPinnedSkills />
     </>
   );
 };
