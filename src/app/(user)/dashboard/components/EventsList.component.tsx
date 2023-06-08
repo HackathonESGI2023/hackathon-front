c75d9064e1d7fb98f896ac28ecc7bdded27f1f2d
Milan <pro@turtletv.fr>
2023-06-08 22:48:18 +0200

    ✨ Add CRUD options

--- src/app/(user)/dashboard/components/EventsList.component.tsx
+++ src/app/(user)/dashboard/components/EventsList.component.tsx
@@ -1,15 +1,42 @@
-import { Card, Container, Spacer } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  Input,
+  Spacer,
+} from "@nextui-org/react";
 import { ActivityType, Event } from "@prisma/client";
 import * as React from "react";
 import { EventItem } from "./EventItem.component";
+import { PencilLine, Trash } from "@phosphor-icons/react";
 
 interface EventsListProps {
   events: Event[];
+  crud?: {
+    onUpdate: (id: number) => void;
+    onDelete: (id: number) => void;
+  };
 }
 
 export const EventsList: React.FunctionComponent<EventsListProps> = ({
   events,
+  crud,
 }) => {
+  const [filteredEvents, setFilteredEvents] = React.useState(events);
+
+  const [search, setSearch] = React.useState("");
+
+  React.useEffect(() => {
+    search === ""
+      ? setFilteredEvents(
+          events.filter((event) =>
+            event.name.toLowerCase().includes(search.toLowerCase())
+          )
+        )
+      : setFilteredEvents(events);
+  }, [search, events]);
+
   return (
     <Card
       variant="flat"
@@ -17,19 +44,78 @@ export const EventsList: React.FunctionComponent<EventsListProps> = ({
         backgroundColor: "#fcfcfc",
       }}
     >
-      <Card.Body
-        css={{
-          overflowY: "auto",
-        }}
-      >
-        <Container>
-          {events.map((event) => (
-            <EventItem
+      <Card.Body>
+        <Input
+          placeholder="Rechercher un évènement..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          size="lg"
+          css={{
+            marginBottom: "1rem",
+          }}
+        />
+        <Container
+          css={{
+            overflowY: "auto",
+          }}
+          gap={0}
+        >
+          {filteredEvents.map((event) => (
+            <Grid.Container
               key={event.id}
-              activityType={event.activityType}
-              date={event.date}
-              name={event.name}
-            />
+              gap={1}
+              css={{
+                marginBottom: "1rem",
+              }}
+            >
+              <Grid xs={12} md={crud ? 10 : 12}>
+                <EventItem
+                  key={event.id}
+                  activityType={event.activityType}
+                  date={event.date}
+                  name={event.name}
+                />
+              </Grid>
+              {crud && (
+                <Grid xs={12} md={2}>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-around",
+                      alignItems: "center",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <Button
+                      color={"warning"}
+                      icon={<PencilLine size={20} />}
+                      auto
+                      flat
+                      onPress={() => crud.onUpdate(event.id)}
+                      css={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    />
+                    <Button
+                      color="error"
+                      onPress={() => crud.onDelete(event.id)}
+                      auto
+                      flat
+                      icon={<Trash size={20} />}
+                      css={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    />
+                  </div>
+                </Grid>
+              )}
+            </Grid.Container>
           ))}
         </Container>
       </Card.Body>
--- src/app/(user)/dashboard/components/MissionsList.component.tsx
+++ src/app/(user)/dashboard/components/MissionsList.component.tsx
@@ -1,16 +1,38 @@
-import { Card, Container } from "@nextui-org/react";
+import { Card, Container, Grid, Input, Button } from "@nextui-org/react";
 import { Company, Mission, User } from "@prisma/client";
 import * as React from "react";
 import { MissionItem } from "./MissionItem.component.tsx";
 import { StuffedMission } from "../../../api/Missions/getMissions.js";
+import { PencilLine, Trash } from "@phosphor-icons/react";
 
 interface MissionsListProps {
   missions: StuffedMission[];
+  crud?: {
+    onUpdate: (id: number) => void;
+    onDelete: (id: number) => void;
+  };
 }
 
 export const MissionsList: React.FunctionComponent<MissionsListProps> = ({
   missions,
+  crud,
 }) => {
+  const [filteredMissions, setFilteredMissions] = React.useState(missions);
+
+  const [search, setSearch] = React.useState("");
+
+  React.useEffect(() => {
+    search === ""
+      ? setFilteredMissions(
+          missions.filter(
+            (m) =>
+              m.name.toLowerCase().includes(search.toLowerCase()) ||
+              m.Company.name.toLowerCase().includes(search.toLowerCase())
+          )
+        )
+      : setFilteredMissions(missions);
+    console.log("Filtered missions", filteredMissions);
+  }, [search, missions]);
   return (
     <Card
       variant="flat"
@@ -18,21 +40,80 @@ export const MissionsList: React.FunctionComponent<MissionsListProps> = ({
         backgroundColor: "#fcfcfc",
       }}
     >
-      <Card.Body
-        css={{
-          overflowY: "auto",
-        }}
-      >
-        <Container>
-          {missions.map((mission) => (
-            <MissionItem
-              companyLogo={mission.Company.logo}
-              missionName={mission.name}
-              companyName={mission.Company.name}
-              startDate={mission.joinDate}
-              endDate={mission.leaveDate}
-              userPicture={mission.Users.profile_picture}
-            />
+      <Card.Body>
+        <Input
+          placeholder="Rechercher une mission..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          size="lg"
+          css={{
+            marginBottom: "1rem",
+          }}
+        />
+        <Container
+          css={{
+            overflowY: "auto",
+          }}
+          gap={0}
+        >
+          {filteredMissions.map((mission) => (
+            <Grid.Container
+              key={mission.id}
+              gap={1}
+              css={{
+                marginBottom: "1rem",
+              }}
+            >
+              <Grid xs={12} md={crud ? 10 : 12}>
+                <MissionItem
+                  companyLogo={mission.Company.logo}
+                  missionName={mission.name}
+                  companyName={mission.Company.name}
+                  startDate={mission.joinDate}
+                  endDate={mission.leaveDate}
+                  userPicture={mission.Users.profile_picture}
+                />
+              </Grid>
+              {crud && (
+                <Grid xs={12} md={2}>
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-around",
+                      alignItems: "center",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  >
+                    <Button
+                      color={"warning"}
+                      icon={<PencilLine size={20} />}
+                      auto
+                      flat
+                      onPress={() => crud.onUpdate(mission.id)}
+                      css={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    />
+                    <Button
+                      color="error"
+                      onPress={() => crud.onDelete(mission.id)}
+                      auto
+                      flat
+                      icon={<Trash size={20} />}
+                      css={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    />
+                  </div>
+                </Grid>
+              )}
+            </Grid.Container>
           ))}
         </Container>
       </Card.Body>
