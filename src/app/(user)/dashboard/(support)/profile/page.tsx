8c350476cbbec27072e180eb0399de448445d847
SylvainBoudacher <sylvain.boudacher@gmail.com>
2023-06-09 13:20:19 +0200

    feat(contract): can select an contract type

--- src/app/(user)/dashboard/(support)/profile/page.tsx
+++ src/app/(user)/dashboard/(support)/profile/page.tsx
@@ -4,52 +4,54 @@ import { Grid, Row } from "@nextui-org/react";
 import { MagnifyingGlass } from "@phosphor-icons/react";
 import { Col, Input, Select } from "antd";
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { getAllUsers } from "src/app/api/Users/getAllUsers";
 import ProfileConsultantCard from "../../components/ProfileConsultantCard/ProfileConsultantCard.component";
 
 const Profile = () => {
   const [users, setUsers] = useState([]);
+  const [searchByNames, setSearchByNames] = useState("");
+  const [selectedContractType, setSelectedContractType] = useState([]);
 
-  const [search, setSearch] = useState("");
-
-  const { data, isLoading } = useQuery(["user"], getAllUsers, {
+  const { data } = useQuery(["user"], getAllUsers, {
     onSuccess: (data) => {
       const filteredData = data.filter((user) =>
         user.roles.includes("CONSULTANT")
       );
       setUsers(filteredData);
+      console.log(filteredData);
     },
   });
 
-  const handleSearchChange = (e) => {
-    const value = e.target.value;
-    setSearch(value);
+  const contractsTypeOptions = [
+    { value: "CDI", label: "CDI" },
+    { value: "CDD", label: "CDD" },
+    { value: "ALTERNANT", label: "ALTERNANT" },
+    { value: "STAGIAIRE", label: "STAGIAIRE" },
+  ];
 
+  const filteredUsers = users.filter(
+    (user) =>
+      (user.firstname.toLowerCase().includes(searchByNames.toLowerCase()) ||
+        user.lastname.toLowerCase().includes(searchByNames.toLowerCase())) &&
+      (selectedContractType.length === 0 ||
+        user?.Contract.some((contract) =>
+          selectedContractType.includes(contract.contractType)
+        ))
+  );
+
+  const handleSearchChangeByNames = (e) => {
+    const value = e.target.value;
+    setSearchByNames(value);
     if (value === "") {
-      // Réinitialiser le filtre lorsque la valeur est vide
       setUsers(data);
     }
   };
 
-  const filteredUsers = users.filter(
-    (user) =>
-      user.firstname.toLowerCase().includes(search.toLowerCase()) ||
-      user.lastname.toLowerCase().includes(search.toLowerCase())
-  );
-
-  useEffect(() => {
-    if (search === "") {
-      setUsers(data);
-    }
-    const filteredUsers = users.filter(
-      (user) =>
-        user.firstname.toLowerCase().includes(search.toLowerCase()) ||
-        user.lastname.toLowerCase().includes(search.toLowerCase())
-    );
-    setUsers(filteredUsers);
-  }, [data, search]);
+  const handleChange = (value: []) => {
+    setSelectedContractType(value);
+  };
 
   return (
     <>
@@ -58,25 +60,21 @@ const Profile = () => {
         <Col span={3} style={{ height: "85vh" }}>
           <Input
             size="small"
-            placeholder="large size"
-            onChange={handleSearchChange}
+            placeholder="Chercher par nom"
+            onChange={handleSearchChangeByNames}
             prefix={
               <MagnifyingGlass size={32} color="#f14e09" weight="light" />
             }
           />
           <Select
-            defaultValue="lucy"
+            mode="tags"
             style={{ width: "100%" }}
-            // onChange={handleChange}
-            options={[
-              { value: "jack", label: "Jack" },
-              { value: "lucy", label: "Lucy" },
-              { value: "Yiminghe", label: "yiminghe" },
-              { value: "disabled", label: "Disabled", disabled: true },
-            ]}
+            placeholder="Type de contrat"
+            onChange={handleChange}
+            options={contractsTypeOptions}
           />
         </Col>
-        <Col style={{ overflowY: "auto", overflowX: "hidden" }}>
+        <Col style={{ overflowY: "auto", overflowX: "hidden", width: "100%" }}>
           <Grid.Container gap={2}>
             {filteredUsers.map((user) => (
               <Grid
@@ -91,7 +89,7 @@ const Profile = () => {
                   fullname={user.firstname + " " + user.lastname}
                   profilePicture={user.profile_picture}
                   isInMission={user.Mission.length > 0}
-                  userContractType={user?.Contract.contractType}
+                  userContractType={user.Contract[0].contractType}
                   pinedSkills={user.UserSkill.filter((skill) => skill.isStarred)
                     .map((skill) => skill.skill)
                     .slice(0, 3)}
