721be2a44da1776791c7b39331b7642c59d91cda
Milan <pro@turtletv.fr>
2023-06-09 03:18:29 +0200

    ♻️ Update w/ files utils

--- src/app/postuler/page.tsx
+++ src/app/postuler/page.tsx
@@ -18,6 +18,7 @@ import { useRef, useState } from "react";
 import { toast } from "react-hot-toast";
 import { useMutation } from "react-query";
 import { createApplication } from "../api/Applications/createApplication";
+import { toBase64 } from "@utils/files.utils";
 
 export default function Postuler() {
   const [name, setName] = useState<string>("");
@@ -92,16 +93,6 @@ export default function Postuler() {
     }
   };
 
-  // Function that convert a file to base54
-  const toBase64 = (file: File): Promise<string> =>
-    new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      // When the file is loaded
-      reader.onload = () => resolve(reader.result as string);
-      // If an error occured
-      reader.onerror = (error) => reject(error);
-    });
   return (
     <>
       <NavbarTest />
