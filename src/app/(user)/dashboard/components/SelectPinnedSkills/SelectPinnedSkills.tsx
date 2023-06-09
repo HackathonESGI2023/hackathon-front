90b5e54311907558196fb5e319d93d0ccd7be40a
Romain Pierucci <pierucci.romain@gmail.com>
2023-06-09 11:49:47 +0200

    feat: modale for select pinned skills

--- src/app/(user)/dashboard/components/SelectPinnedSkills/SelectPinnedSkills.tsx
+++ src/app/(user)/dashboard/components/SelectPinnedSkills/SelectPinnedSkills.tsx
@@ -2,13 +2,15 @@
 
 import SkillBadge from "@components/SkillBadge/SkillBadge";
 import SkillBadgeSlot from "@components/SkillBadgeSlot/SkillBadgeSlot";
-import { Row } from "@nextui-org/react";
+import { Button, Modal, Row, Text } from "@nextui-org/react";
 import { useState } from "react";
+import styles from "./SelectPinnedSkills.module.scss";
 
 type SelectPinnedSkillsProps = {};
 
 const SelectPinnedSkills = ({}: SelectPinnedSkillsProps) => {
   const [slots, setSlots] = useState([null, null, null]);
+  const [isVisible, setIsVisible] = useState<boolean>(false);
 
   const [badges, setBadges] = useState([
     { id: 1, name: "React", color: "red", slot: null },
@@ -47,36 +49,64 @@ const SelectPinnedSkills = ({}: SelectPinnedSkillsProps) => {
 
   return (
     <>
-      <Row align="center">
-        {badges
-          .filter((badge) => badge.slot === null)
-          .map((badge) => (
-            <SkillBadge
-              key={badge.id}
-              name={badge.name}
-              color={badge.color}
-              id={badge.id}
-              onDrop={handleDrop}
-            />
-          ))}
-      </Row>
-      <Row align="center">
-        {slots.map((slotId, index) => (
-          <SkillBadgeSlot key={index} onDrop={handleDrop} id={index}>
-            {badges
-              .filter((badge) => badge.slot === index)
-              .map((badge) => (
-                <SkillBadge
-                  key={badge.id}
-                  name={badge.name}
-                  color={badge.color}
-                  id={badge.id}
-                  onDrop={handleDrop}
-                />
+      <Button onClick={() => setIsVisible(true)}>pinned skills</Button>
+      <Modal
+        scroll
+        fullScreen
+        closeButton
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        onClose={() => setIsVisible(false)}
+        open={isVisible}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Mettez en valeur 3 compétences sur votre profil
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <div className={styles.container}>
+            <Row align="center">
+              {badges
+                .filter((badge) => badge.slot === null)
+                .map((badge) => (
+                  <SkillBadge
+                    key={badge.id}
+                    name={badge.name}
+                    color={badge.color}
+                    id={badge.id}
+                    onDrop={handleDrop}
+                  />
+                ))}
+            </Row>
+            <Row align="center">
+              {slots.map((slotId, index) => (
+                <SkillBadgeSlot key={index} onDrop={handleDrop} id={index}>
+                  {badges
+                    .filter((badge) => badge.slot === index)
+                    .map((badge) => (
+                      <SkillBadge
+                        key={badge.id}
+                        name={badge.name}
+                        color={badge.color}
+                        id={badge.id}
+                        onDrop={handleDrop}
+                      />
+                    ))}
+                </SkillBadgeSlot>
               ))}
-          </SkillBadgeSlot>
-        ))}
-      </Row>
+            </Row>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onPress={() => setIsVisible(false)}>
+            Close
+          </Button>
+          <Button auto onPress={() => setIsVisible(false)}>
+            Agree
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
