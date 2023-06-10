e8c8aa6742a225951151371303b7208d5e1c322d
Milan <pro@turtletv.fr>
2023-06-10 18:40:02 +0200

    👽️ Refact api name

--- src/app/api/Slack/getUsers.ts
+++ src/app/api/Slack/getUsers.ts
@@ -11,7 +11,7 @@ export interface SlackMessageRequest {
   message: string;
 }
 
-export const sendMessage = async (): Promise<UsersListResponse> => {
+export const getSlackUsers = async (): Promise<UsersListResponse> => {
   const res = await GET(`slack/users`);
 
   return res.json();
